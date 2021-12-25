89c19c5c7645dd70435f0f4577e083497821711d
Satish Babariya <satish.babariya@gmail.com>
2021-12-25 14:49:50 +0530

    added port

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"os"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	e := echo.New()
 
 	e.Use(middleware.Logger())
@@ -14,5 +21,5 @@ func main() {
 	// Routes
 
 	// Start server
-	e.Logger.Fatal(e.Start(":3000"))
+	e.Logger.Fatal(e.Start(":" + port))
 }
